8aaf020ab3862d20b5114f5aacfb4ba31ee6cb19
YelovSK <hampatrik@gmail.com>
2022-01-15 13:16:31 +0100

    ops

--- JournalFind.py
+++ JournalFind.py
@@ -12,7 +12,6 @@ class Journal:
         self.files = list(os.listdir(self.path))
         self.years = self.get_years()
         self.console = Console()
-        self.word_count_list = []
         self.word_count_dict = {}
         self.files_list_path = os.path.join(self.base, "files.txt")
         self.check_file_count_mismatch()
@@ -35,21 +34,17 @@ class Journal:
     def init_dict(self):
         try:
             self.read_dict()
-        except KeyError:
-            self.write_dict()
-        except FileNotFoundError:
-            pathlib.Path(os.path.join(self.base, "shelve")).mkdir(parents=True, exist_ok=True)
+        except (KeyError, FileNotFoundError):
             self.write_dict()
 
     def read_dict(self):
         with shelve.open(os.path.join(self.base, "shelve", "journal")) as jour:
-            self.word_count_list = jour["words"]
             self.word_count_dict = jour["freq"]
 
     def write_dict(self):
         self.create_word_frequency()
+        pathlib.Path(os.path.join(self.base, "shelve")).mkdir(parents=True, exist_ok=True)
         with shelve.open(os.path.join(self.base, "shelve", "journal")) as jour:
-            jour["words"] = self.word_count_list
             jour["freq"] = self.word_count_dict
             
     def create_word_frequency(self):
@@ -59,7 +54,6 @@ class Journal:
                 file_content_list.append(f.read())
         content = "".join(file_content_list).lower()
         self.word_count_dict = Counter(re.findall("\w+", content))
-        self.word_count_list = sorted(self.word_count_dict.items(), key=lambda x: x[1], reverse=True)
 
     def get_years(self):
         YEAR_START_IX = 8
@@ -94,8 +88,8 @@ class Journal:
         with open(self.files_list_path, "w") as f:
             f.write(str(len(self.files)))
 
-    def get_most_frequent_words(self, count=20):
-        return self.word_count_list[:count]
+    def get_most_frequent_words(self, count):
+        return sorted(self.word_count_dict.items(), key=lambda item: item[1], reverse=True)[:count]
 
     def get_unique_word_count(self):
         return len(self.word_count_dict)
@@ -116,7 +110,7 @@ class Journal:
             for line in f:
                 english_words.add(line.strip())
         all_words_count = self.get_total_word_count()
-        english_word_count = sum(count for word, count in self.word_count_list if word in english_words)
+        english_word_count = sum(count for word, count in self.word_count_dict.items() if word in english_words)
         self.console.print(f"All words: {all_words_count} | English word count: {english_word_count}")
         self.console.print(f"Percentage of english words: {round(english_word_count*100 / all_words_count, 3)}%")
 
@@ -165,7 +159,7 @@ class Journal:
                 self.console.print("Unique words count:", self.get_unique_word_count())
                 if not val or not val.isnumeric():
                     val = 10
-                self.console.print(self.get_most_frequent_words(int(int(val))))
+                self.console.print(self.get_most_frequent_words(int(val)))
             elif action == "-c":
                 self.console.print(f"The exact match of word '{val}' was found {self.word_count_dict[val]} times")
                 self.find(val, False)
